16e3acdee31c353d96ee7edaa2f0dec2d48453de
Norbert de Langen <ndelangen@me.com>
2023-02-13 15:06:27 +0100

    forward NODE_OPTIONS

--- scripts/tasks/sandbox-parts.ts
+++ scripts/tasks/sandbox-parts.ts
@@ -119,10 +119,20 @@ export const install: Task['run'] = async ({ sandboxDir, template }, { link, dry
   });
 
   logger.info(`🔢 Adding package scripts:`);
+
+  const nodeOptions = [
+    ...process.env.NODE_OPTIONS.split(' '),
+    '--preserve-symlinks',
+    '--preserve-symlinks-main',
+  ];
+
+  const prefix = `NODE_OPTIONS="${nodeOptions.join(
+    ' '
+  )}" STORYBOOK_TELEMETRY_URL="http://localhost:6007/event-log"`;
+
   await updatePackageScripts({
     cwd,
-    prefix:
-      'NODE_OPTIONS="--preserve-symlinks --preserve-symlinks-main" STORYBOOK_TELEMETRY_URL="http://localhost:6007/event-log"',
+    prefix,
   });
 
   switch (template.expected.framework) {
